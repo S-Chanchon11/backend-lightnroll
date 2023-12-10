5e71f44955348139f89940799330dfd3303a80e8
Chanchon Saisema <chanchon.splus@gmail.com>
2023-12-10 09:35:56 +0700

    add sample audio and description

--- functions/app/audio_processing.py
+++ functions/app/audio_processing.py
@@ -16,13 +16,25 @@ class PreAudioProcess:
         print(y)
         print(sr)
 
+    # get time series of fundamental frequencies in Hertz.
     def pitch_recognition(self):
-        y, sr = librosa.load(librosa.ex('trumpet'))
+
+        # :-------- Warning! ---------:
+        # .load() not support .m4a file
+
+        y, sr = librosa.load('/Users/snow/backend-lightnroll/functions/app/dataset/c_maj.m4a')
+
         f0, voiced_flag, voiced_probs = librosa.pyin(y,
                                                      fmin=librosa.note_to_hz('C2'),
                                                      fmax=librosa.note_to_hz('C7'))
-        times = librosa.times_like(f0)
-        print(f0)
+        # times = librosa.times_like(f0)
+        
+        
+        # note = str(type(f0[0]))
+        toFloat = f0.tolist()
+        note = librosa.hz_to_note(toFloat[2])
+
+        print(note)
 
     def dynamics_recognition(self):
         y, sr = librosa.load(librosa.ex('trumpet'))
@@ -35,8 +47,8 @@ def main():
 
     pap = PreAudioProcess()
     #app.loading_process()
-    #pap.pitch_recognition()
-    pap.dynamics_recognition()
+    pap.pitch_recognition()
+    #pap.dynamics_recognition()
 
 if __name__ == '__main__':
     main()
